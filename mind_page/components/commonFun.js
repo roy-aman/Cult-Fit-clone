18c8db8b0d12606166f6e95b0ff2aa44597d26de
Aman Roy <cs.amaroy@gmail.com>
2022-03-30 11:57:16 +0530

    1st commit

---
+++ mind_page/components/commonFun.js
@@ -0,0 +1,34 @@
+
+const mHead= ()=>{
+
+    return ` <button>Therapy</button>
+    <button>MindFulness</button>`;
+}
+
+
+
+const getMindExperts = ()=>{
+
+    let exports =[
+       { title : "Transform Your Thinking", des : "Manage negative or anxious thoughts or overthinking ₹699", time:"50 Mins Session",image:"https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_580,ar_2.279,c_fit/dpr_2/image/vm/dcd1728e-d7dc-4952-b552-b1b1c97e9ac1.png"},
+    
+       { title : "Healthy Relationships", des : "Understand your relationship patterns and exploring healthy ways of connecting ₹699", time:"50 Mins Session", image:"https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_580,ar_2.279,c_fit/dpr_2/image/vm/1a2d8f7e-2c24-4cbf-88be-e2cf266dfa23.png"},
+
+       { title : "Thrive at work", des : "Make work fulfilling and meaningful with the help of our experts ₹699", time:"50 Mins Session",image:"https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_580,ar_2.279,c_fit/dpr_2/image/vm/18a8d3b2-d262-4f78-908b-79a55a40ef68.png"},
+       
+       { title : "Transform Your Thinking", des : "Manage negative or anxious thoughts or overthinking ₹699", time:"50 Mins Session",image:"https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_580,ar_2.279,c_fit/dpr_2/image/vm/aebc57c2-ee58-41f7-a854-00fb5c5d55f9.png"},
+
+       { title : "Better Sleep", des : "Consult with experts to learn effective strategies for restful sleep and good sleep", time:"50 Mins Session", image:"https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_580,ar_2.279,c_fit/dpr_2/image/vm/84248218-f8c0-44d8-b2d6-be415b742b9a.png"},
+
+       { title : "COVID Stress", des : "Seek expert help from our therapists for coping with isolation and COVID-related", time:"50 Mins Session", image:"https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_580,ar_2.279,c_fit/dpr_2/image/vm/5e9f8ee5-83dd-4dca-beca-61885323ce3c.png"},
+
+       { title : "Couple Therapy", des : "Resolve emotional conflicts in relationships and marriage ₹699", time:"50 Mins Session", image:"https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_580,ar_2.279,c_fit/dpr_2/image/vm/9ce1d9a2-d3c6-4a19-91bd-028d52f58c84.png"},
+
+       { title : "Therapy", des : "Experts who help solve emotional problems ₹699", time:"50 Mins Session", image:"https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_580,ar_2.279,c_fit/dpr_2/image/vm/9355d11a-f088-4f10-b9df-72ea3ecf8e0b.png"},
+    ];
+    return exports;
+}
+
+
+
+export { mHead, getMindExperts};
