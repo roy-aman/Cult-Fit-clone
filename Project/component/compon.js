af229d60f08b64a189a21200019d3fd60b1f051f
PRATIK JAWANJAL <99707698+ImErPratik@users.noreply.github.com>
2022-04-01 00:07:27 +0530

    Add files via upload

---
+++ Project/component/compon.js
@@ -0,0 +1,91 @@
+const addFAQ= ()=>{
+
+    return ` 
+    <h1>FAQS</h1>
+    <h1>some important questions</h1>
+    <div class="faq-container">
+        <div class="faq">
+            <h3 class="faq-title"> Is therapy for me?</h3>
+            <p class="faq-text">
+            Whether you are experiencing symptoms that come in the way of daily living or want to explore issues 
+            experienced in the past or are keen on enhancing your wellbeing and growth in life - therapy is suitable for all.
+            </p>
+            <button class="faq-toggle">
+                <i class="fas fa-chevron-down"></i>
+                <i class="fas fa-times"></i>
+            </button>
+        </div>
+        <div class="faq">
+            <h3 class="faq-title">What should I expect during my first therapy session?</h2>
+            <p class="faq-text">
+            Your therapist will focus on getting to know you better through the first session.
+             Together, the two of you will identify goals for follow up sessions.
+             Your therapist may also share a few exercises for you to practice, if relevant.
+            </p>
+            <button class="faq-toggle">
+                <i class="fas fa-chevron-down"></i>
+                <i class="fas fa-times"></i>
+            </button>
+        </div>
+        <div class="faq">
+            <h3 class="faq-title">What is the difference between a Therapist and a Psychiatrist?</h3>
+            <p class="faq-text">
+            Therapists go deep into our problems with us. They help us understand ourselves better and plan for recovery without using medicines.
+             Psychiatrists diagnose and treat chronic mental health conditions and may use medicines. 
+            </p>
+            <button class="faq-toggle">
+                <i class="fas fa-chevron-down"></i>
+                <i class="fas fa-times"></i>
+            </button>
+        </div>
+        <div class="faq">
+            <h3 class="faq-title">What does a Therapy pack offer?</h3>
+            <p class="faq-text">
+            A Therapy pack helps with continuity of your wellness journey,
+             without having to bother about booking sessions one at a time.
+              Your Therapist will recommend the number of sessions that you might need to address your challenges.
+             We recommend you buy a pack only after a discussion with the therapist. 
+            </p>
+            <button class="faq-toggle">
+                <i class="fas fa-chevron-down"></i>
+                <i class="fas fa-times"></i>
+            </button>
+        </div>
+        <div class="faq">
+            <h3 class="faq-title">How to book a 1 on 1 Therapy session?</h3>
+            <p class="faq-text">
+            Here’s how you can book a therapy session from the cure.fit app: Log into the cure.
+            fit app From the bottom of the To-Do page, pick mind.fit Pick 1 on 1 therapy Pick a preferred center and time slot Make a payment That’s all!
+            </p>
+            <button class="faq-toggle">
+                <i class="fas fa-chevron-down"></i>
+                <i class="fas fa-times"></i>
+            </button>
+        </div>
+        <div class="faq">
+            <h3 class="faq-title">Who all will have access to my medical data?</h3>
+            <p class="faq-text">
+            Only your assigned Therapist and/or assigned Psychiatrist will
+             have access to your medical data until you provide explicit access to any other professional
+            </p>
+            <button class="faq-toggle">
+                <i class="fas fa-chevron-down"></i>
+                <i class="fas fa-times"></i>
+            </button>
+        </div>
+        <div class="faq">
+            <h3 class="faq-title">The information I share with my therapist is sensitive information, is this information safe?
+            </h3>
+            <p class="faq-text">
+            Absolutely! We ensure none of your medical/psychiatric information is shared with anyone but your Therapist and Psychiatrist.
+             All the information you share with us is completely safe and secure with us.
+            </p>
+            <button class="faq-toggle">
+                <i class="fas fa-chevron-down"></i>
+                <i class="fas fa-times"></i>
+            </button>
+        </div>
+    </div>`
+}
+
+export { addFAQ};
