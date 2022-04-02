9c5894d4d1c2c44e2be1af19d1d08e701c03f137
ImErPratik <99707698+ImErPratik@users.noreply.github.com>
2022-04-02 23:37:01 +0530

    update on control overflow

--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,7 @@ const navbar = () => {
     <div class="navbar_logo">
       <a href="./"><img
         src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_120,ar_3.87,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/brand-logo/vman-and-white-cult-text.png"
-        alt="logo"
+        alt="logo" id="return_home"
       /> </a>
     </div>
     <div class="middle_nav_links">
