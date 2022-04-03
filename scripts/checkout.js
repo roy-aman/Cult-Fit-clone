ddcd4126a5a36dc38e055c0673192e17cfdaf94b
ImErPratik <99707698+ImErPratik@users.noreply.github.com>
2022-04-03 14:01:53 +0530

    adress form

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -60,7 +60,9 @@ const increaseqty = (el) => {
 }
 
 const decreaseqty = (el) => {
-   if(el.qty === 0) {
+   if(el.qty === 1) {
+      removefromcart(cartdata,el)
+
       pricecal();
       return
    }
@@ -153,4 +155,63 @@ const display = (cartdata) => {
 }
 
 display(cartdata)
+
+
+
+
+// for adress --------------------------
+const adress = document.querySelector("#adres");
+// const close_login = document.querySelector(".close_login");
+
+adress.addEventListener("click", () => {
+  show_address_form();
+});
+
+
+const show_address_form = ()=>{
+   const adress = document.querySelector("#addres");
+   adress.innerHTML=`<div>
+   <div class="row">
+      <div class="col-75">
+        <div class="container">
+          <form action="/action_page.php">
+    
+            <div class="row">
+              <div class="col-50">
+                <h3>Billing Address</h3>
+                <label for="fname"><i class="fa fa-user"></i> Full Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="John M. Doe">
+                <label for="email"><i class="fa fa-envelope"></i> Email</label>
+                <input type="text" id="email" name="email" placeholder="john@example.com">
+                <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+                <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
+                <label for="city"><i class="fa fa-institution"></i> City</label>
+                <input type="text" id="city" name="city" placeholder="New York">
+    
+                <div class="row">
+                  <div class="col-50">
+                    <label for="state">State</label>
+                    <input type="text" id="state" name="state" placeholder="NY">
+                  </div>
+                  <div class="col-50">
+                    <label for="zip">Zip</label>
+                    <input type="text" id="zip" name="zip" placeholder="10001">
+                  </div>
+                </div>
+              </div>
+    
+    
+            </div>
+            <label>
+              <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
+            </label>
+          </form>
+        </div>
+      </div>
+   </div>
+</div>`
+}
+
+   
+
 
